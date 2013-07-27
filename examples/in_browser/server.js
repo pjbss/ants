c0f89204a7e02c0d57c0cd9324c48d928ab26d69
Patrick Sexton <onethreelabs@gmail.com>
2013-07-27 17:22:05 -0500

    adding some more stuff

--- examples/in_browser/server.js
+++ examples/in_browser/server.js
@@ -1,24 +1,11 @@
 var http = require('http');
 var url = require('url');
-var Ants = require('../../lib/AntFarm');
+var NodeStatic = require('node-static');
 
-var farm = new Ants();
-
-farm.addNode('a', function(p){
-    var url_parts = url.parse(p, true);
-    return url_parts.path;
-});
-
-farm.addNode('b', function(p){
-    return 'No ' + p + ' For YOU!!!';
-});
-farm.connect('a', 'b');
+var fileServer = new NodeStatic.Server('./static');
 
 http.createServer(function (req, res) {
-    farm.message(req.url, function(endPacket){
-        res.writeHead(200, {'Content-Type': 'text/plain'});
-        res.end(endPacket + '\n');
-    }, {});
+    fileServer.serve(req,res);
 }).listen(1337, '127.0.0.1');
 
 console.log('Server running at http://127.0.0.1:1337/');
